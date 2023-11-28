159f1e05e7f007bffee1180d5ae988f50cd5802a
aajusah98 <ajaysah531@gmail.com>
2023-11-28 17:36:57 +0100

    fixting string error in exercise 2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.types import *
 import re
-
+import sys
 
 # Fetch the CSV data from the provided URL
 data_set_url='https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
@@ -12,54 +13,41 @@ df = pd.read_csv(data_set_url, delimiter=';')
 # Drop the "Status" column
 df = df.drop(columns=['Status'])
 
-# Define validation functions
-def is_valid_verkehr(value):
-    return value in ["FV", "RV", "nur DPN"]
-
-def is_valid_coordinate(value):
-    try:
-        # Replace comma with dot and convert to float
-        numeric_value = float(value.replace(',', '.'))
-        return -90 <= numeric_value <= 90
-    except (ValueError, TypeError):
-        return False
-
 def is_valid_ifopt(value):
     # Regular expression pattern for IFOPT validation
     pattern = re.compile(r"^\w{2}:\d+:\d+(?::\d+)?$")
     return bool(pattern.match(str(value)))
 
 # Apply data validation and filtering
-df = df[df['Verkehr'].apply(is_valid_verkehr)]
-df = df[df['Laenge'].apply(is_valid_coordinate)]
-df = df[df['Breite'].apply(is_valid_coordinate)]
+df['Laenge'] = df['Laenge'].str.replace(',', '.').astype(float)
+df['Breite'] = df['Breite'].str.replace(',', '.').astype(float)
+df = df[ (df['Laenge'] <= 90) & (df['Laenge'] >= -90)]
+df = df[ (df['Breite'] <= 90) & (df['Breite'] >= -90)]
+df = df[df['Verkehr'].isin(['FV', 'nur DPN', 'RV'])]
 df = df[df['IFOPT'].apply(is_valid_ifopt)]
 df = df.dropna()  # Drop rows with empty cells
 
 # print(df['Laenge'])
 # sys.exit()
 # Define column data types
-column_types = {
-    "EVA_NR": int,
-    "DS100": str,
-    "IFOPT": str,
-    "NAME": str,
-    "Verkehr": str,
-    "Laenge": str,
-    "Breite": str,
-    "Betreiber_Name": str,
-    "Betreiber_Nr": int
+columns_type={
+    "EVA_NR": Integer,
+    "DS100": Text,
+    "IFOPT": Text,
+    "NAME": Text,
+    "Verkehr": Text,
+    "Laenge": Float,
+    "Breite": Float,
+    "Betreiber_Name": Text,
+    "Betreiber_Nr": Integer,
 }
 
 # Connect to SQLite database
 # conn = sqlite3.connect('exercises/trainstops.sqlite')
 conn=create_engine("sqlite:///trainstops.sqlite", echo=True)
 
-df = df.astype(column_types)
 # Write the DataFrame to SQLite database
-df.to_sql('trainstops', conn, if_exists='replace',index=False)
+df.to_sql('trainstops', conn, if_exists='replace',index=False,dtype=columns_type)
 
 # Close the database connection
-
-
 print("DONE")
