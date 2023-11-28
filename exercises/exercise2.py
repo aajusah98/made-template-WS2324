5386d33016c3b25be64f1e10ee61edc371f3fd21
Ajay Sah <ajay.sah@fau.de>
2023-11-28 13:11:33 +0100

    renaming name for exercise2

---
+++ exercises/exercise2.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import sqlite3
+import re
+
+
+# Fetch the CSV data from the provided URL
+data_set_url='https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
+
+# Read CSV data into a pandas DataFrame
+df = pd.read_csv(data_set_url, delimiter=';')
+
+# Drop the "Status" column
+df = df.drop(columns=['Status'])
+
+# Define validation functions
+def is_valid_verkehr(value):
+    return value in ["FV", "RV", "nur DPN"]
+
+def is_valid_coordinate(value):
+    try:
+        # Replace comma with dot and convert to float
+        numeric_value = float(value.replace(',', '.'))
+        return -90 <= numeric_value <= 90
+    except (ValueError, TypeError):
+        return False
+
+def is_valid_ifopt(value):
+    # Regular expression pattern for IFOPT validation
+    pattern = re.compile(r"^\w{2}:\d+:\d+(?::\d+)?$")
+    return bool(pattern.match(str(value)))
+
+# Apply data validation and filtering
+df = df[df['Verkehr'].apply(is_valid_verkehr)]
+df = df[df['Laenge'].apply(is_valid_coordinate)]
+df = df[df['Breite'].apply(is_valid_coordinate)]
+df = df[df['IFOPT'].apply(is_valid_ifopt)]
+df = df.dropna()  # Drop rows with empty cells
+
+# print(df.dtypes)
+# sys.exit()
+# Define column data types
+column_types = {
+    "EVA_NR": "INTEGER",
+    "DS100": "TEXT",
+    "IFOPT": "TEXT",
+    "NAME": "TEXT",
+    "Verkehr": "TEXT",
+    "Laenge": "REAL",
+    "Breite": "REAL",
+    "Betreiber_Name": "TEXT",
+    "Betreiber_Nr": "REAL"
+}
+
+# Connect to SQLite database
+conn = sqlite3.connect('exercises/trainstops.sqlite')
+
+# Write the DataFrame to SQLite database
+df.to_sql('trainstops', conn, index=False, if_exists='replace', dtype=column_types)
+
+# Close the database connection
+conn.close()
