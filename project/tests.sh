46c9739eb798eb7157445990cab73777c18c0cdf
aajusah98 <ajaysah531@gmail.com>
2024-01-16 13:33:15 +0100

    Update directory path

--- project/tests.sh
+++ project/tests.sh
@@ -3,7 +3,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Export kaggle.json to os env for Kaggle authentication
-KAGGLE_JSON_PATH="./kaggle.json"
+KAGGLE_JSON_PATH="./project/kaggle.json"
 KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
 export KAGGLE_CONFIG_DIR
 
